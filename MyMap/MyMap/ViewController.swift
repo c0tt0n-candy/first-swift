979040677dee6240b0390fbf695c305ec43cef95
yokubo <utsue.38@gmail.com>
2019-01-02 16:28:44 +0900

    implement get current location & draw map view

--- MyMap/MyMap/ViewController.swift
+++ MyMap/MyMap/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let manager = CLLocationManager()
     @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        if (CLLocationManager.authorizationStatus() != .authorizedWhenInUse) {
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if (status == .authorizedWhenInUse) {
+            manager.requestLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("locations : \(locations)")
+        let current = locations[0]
+        let region = MKCoordinateRegion.init(center: current.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(region, animated: true)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error = \(error)")
     }
 }
