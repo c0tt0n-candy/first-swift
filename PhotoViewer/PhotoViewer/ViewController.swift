ec1210e2d3e6443ded92f4077f9907a8cc993f30
yokubo <utsue.38@gmail.com>
2019-01-03 16:47:47 +0900

    create image list view

--- PhotoViewer/PhotoViewer/ViewController.swift
+++ PhotoViewer/PhotoViewer/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
